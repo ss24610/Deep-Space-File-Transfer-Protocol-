c21ae119c7c7ba6c1d16ce5ce34cc918667e3782
ss24610 <salmanmohammadsaif@gmail.com>
2026-02-16 15:46:18 -0500

    added code for sender/receiver

--- Receiver/Receiver.java
+++ Receiver/Receiver.java
@@ -1,4 +1,129 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
-public class Receiver {
+public class Receiver{
+
+    public static void main(String[] args) {
+
+        if(args.length < 5) {
+            System.out.println("Error missing required arguments");
+            return;
+        }
     
+        String sender_ip = args[0];
+        int sender_ack_port = Integer.parseInt(args[1]);
+        int rcv_data_port = Integer.parseInt(args[2]);
+        String output_file = args[3];
+        int rn = Integer.parseInt(args[4]);
+    
+        int expected_seq;
+
+        try {
+    
+            DatagramSocket receiver_socket = new DatagramSocket(rcv_data_port);
+    
+            byte[] buffer = new byte[DSPacket.MAX_PACKET_SIZE];
+            DatagramPacket received_datagram = new DatagramPacket(buffer, buffer.length);
+            
+            while(true){
+
+                receiver_socket.receive(received_datagram);
+                
+                DSPacket packet = new DSPacket(received_datagram.getData());
+
+                if(packet.getSeqNum()==0 && packet.getType()==DSPacket.TYPE_SOT){
+                    System.out.println("SOT received from Sender");
+                    DSPacket ACK_packet = new DSPacket(DSPacket.TYPE_ACK, 0, null);
+                    byte[] raw_bytes = ACK_packet.toBytes();
+        
+                    DatagramPacket ACK_datagram = new DatagramPacket(
+                        raw_bytes,
+                        raw_bytes.length,
+                        received_datagram.getAddress(),
+                        sender_ack_port
+                    );
+        
+                    System.out.println("Sending ACK packet");
+                    expected_seq = 1;
+                    // open file
+                    receiver_socket.send(ACK_datagram);
+        
+                }
+
+                else if (expected_seq == packet.getSeqNum()) {
+
+                    if(packet.getType()==DSPacket.TYPE_EOT) {
+
+                        // teardown
+
+                        DSPacket ACK_packet = new DSPacket(DSPacket.TYPE_EOT, expected_seq, null);
+                        byte[] raw_bytes = ACK_packet.toBytes();
+            
+                        DatagramPacket ACK_datagram = new DatagramPacket(
+                            raw_bytes,
+                            raw_bytes.length,
+                            received_datagram.getAddress(),
+                            sender_ack_port
+                        );
+
+                        // close file
+                        receiver_socket.send(ACK_datagram);
+                        receiver_socket.close();
+                        break;
+
+                    }
+
+                    else if(packet.getType()==DSPacket.TYPE_DATA) {
+
+                        DSPacket ACK_packet = new DSPacket(DSPacket.TYPE_ACK, expected_seq, null);
+                        byte[] raw_bytes = ACK_packet.toBytes();
+            
+                        DatagramPacket ACK_datagram = new DatagramPacket(
+                            raw_bytes,
+                            raw_bytes.length,
+                            received_datagram.getAddress(),
+                            sender_ack_port
+                        );
+
+                        receiver_socket.send(ACK_datagram);
+                        expected_seq ++;
+                        // write data to file
+                        
+
+                    }
+
+                }
+
+                else {
+                
+                    // send ACK for last received seq num
+                    DSPacket ACK_packet = new DSPacket(DSPacket.TYPE_ACK, expected_seq-1, null);
+                    byte[] raw_bytes = ACK_packet.toBytes();
+        
+                    DatagramPacket ACK_datagram = new DatagramPacket(
+                        raw_bytes,
+                        raw_bytes.length,
+                        received_datagram.getAddress(),
+                        sender_ack_port
+                    );
+                    receiver_socket.send(ACK_datagram);
+
+                }
+
+            }
+        
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    
+    
+    }
+    
+
 }
+
+
--- Sender/Sender.java
+++ Sender/Sender.java
@@ -1,5 +1,212 @@
-package Deep-Space-File-Transfer-Protocol-.Sender;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
 public class Sender {
+
+    public static void main(String[] args) {
+
+        if(args.length < 5){
+            System.out.println("Error missing required arguments");
+            return;
+        }
+    
+        String rcv_ip = args[0];
+        int rcv_data_port = Integer.parseInt(args[1]);
+        int sender_ack_port = Integer.parseInt(args[2]);
+        String input_file = args[3];
+        int timeout_ms = Integer.parseInt(args[4]);
+
+        FileInputStream input_stream = null;
+        DatagramSocket sender_socket = null;
+    
+        // Stop and weight
+        if(args.length == 5) {
+
+            try {
+                
+                sender_socket = new DatagramSocket(sender_ack_port);
+                sender_socket.setSoTimeout(timeout_ms);
+
+                DSPacket SOT_packet = new DSPacket(DSPacket.TYPE_SOT, 0, null);
+                byte[] raw_bytes = SOT_packet.toBytes();
+    
+                DatagramPacket UDP_datagram = new DatagramPacket(
+                    raw_bytes,
+                    raw_bytes.length,
+                    InetAddress.getByName(rcv_ip),
+                    rcv_data_port
+                );
+
+                int timeout_count = 0;
+
+                // initial handshake
+                while (timeout_count < 3){
+
+                    System.out.println("Sending SOT packet");
+                    sender_socket.send(UDP_datagram);
+
+                    try {
+        
+                        byte[] buffer = new byte[DSPacket.MAX_PACKET_SIZE];
+                        DatagramPacket ackUDP = new DatagramPacket(buffer, buffer.length);
+            
+                        sender_socket.receive(ackUDP);
+                        System.out.println("Received ACK packet");
+        
+            
+                        DSPacket ackPacket = new DSPacket(ackUDP.getData());
+            
+                        if(ackPacket.getType() == DSPacket.TYPE_ACK && ackPacket.getSeqNum() == 0) {
+            
+                            System.out.println("Handshake complete!");
+                            break;
+                        }
+            
+            
+            
+                    } catch (SocketTimeoutException  e) {
+                        // TODO: handle exception
+                        timeout_count ++;
+                        System.out.println("Timeout waiting for ACK (" + timeout_count + "/3)");
+                    }
+
+                }
+
+                if(timeout_count==3){
+                    System.out.println("Error, failed to complete handshake");
+                    return;
+                }
+
+                // open file
+                int sequence_number = 1;
+                timeout_count=0;
+
+                byte[] buffer = new byte[DSPacket.MAX_PACKET_SIZE];
+                DatagramPacket ackUDP = new DatagramPacket(buffer, buffer.length);
+                sender_socket.setSoTimeout(timeout_ms);
+
+                input_stream = new FileInputStream(input_file);
+                byte[] file_buffer = new byte[DSPacket.MAX_PAYLOAD_SIZE];
+
+
+                while(true) {
+                    int bytes_read = input_stream.read(file_buffer);
+
+                    if(bytes_read==-1){
+                        break;
+                    }
+
+                    timeout_count = 0;
+
+                    byte[] payload = Arrays.copyOf(file_buffer, bytes_read);
+
+                    DSPacket DATA_packet = new DSPacket(DSPacket.TYPE_DATA, sequence_number, payload);
+                    raw_bytes = DATA_packet.toBytes();
+        
+                    UDP_datagram = new DatagramPacket(
+                        raw_bytes,
+                        raw_bytes.length,
+                        InetAddress.getByName(rcv_ip),
+                        rcv_data_port
+                    );
+
+                    
+
+                    // maybe loop through file 124 bytes per time
+                    while(timeout_count < 3){
+                        sender_socket.send(UDP_datagram);
+
+                        try {
+                        
+                            // read max 124 bytes from file, put this in data and replace for null
+                        
+                            
+
+                            sender_socket.receive(ackUDP);
+                            DSPacket ackPacket = new DSPacket(ackUDP.getData());
+                
+                            if(ackPacket.getType() == DSPacket.TYPE_ACK && ackPacket.getSeqNum() == sequence_number) {
+                
+                                System.out.println("ACK Received!");
+                                sequence_number ++;
+                            }
+    
+                        } catch (SocketTimeoutException  e) {
+                            // TODO: handle exception
+                            timeout_count ++;
+                            System.out.println("Timeout waiting for ACK (" + timeout_count + "/3)");
+                        }
+
+                    }
+
+                    if (timeout_count == 3) {
+                        System.out.println("Transfer failed.");
+                        input_stream.close();
+                        break;
+                    }
+                    
+                }
+
+                // send EOF when the file is done and then break from loops
+
+                DSPacket EOT_packet = new DSPacket(DSPacket.TYPE_EOT, sequence_number, null);
+                raw_bytes = EOT_packet.toBytes();
+                timeout_count = 0;
+    
+                UDP_datagram = new DatagramPacket(
+                    raw_bytes,
+                    raw_bytes.length,
+                    InetAddress.getByName(rcv_ip),
+                    rcv_data_port
+                );
+
+                while(timeout_count < 3){
+                    sender_socket.send(UDP_datagram);
+
+                        try {
+                        
+                            sender_socket.receive(ackUDP);
+                            DSPacket ackPacket = new DSPacket(ackUDP.getData());
+                
+                            if(ackPacket.getType() == DSPacket.TYPE_EOT && ackPacket.getSeqNum() == sequence_number) {
+        
+                                System.out.println("EOT Received!");
+                                break;
+                            }
+    
+                        } catch (SocketTimeoutException  e) {
+                            // TODO: handle exception
+                            timeout_count ++;
+                            System.out.println("Timeout waiting for EOT ACK (" + timeout_count + "/3)");
+                        }
+
+                }
+
+
+                // print the total time
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println(e.getStackTrace());
+            } finally {
+
+                if(input_stream != null) input_stream.close();
+                if(sender_socket != null) sender_socket.close();
+            }
+
+        }
+    
+    
+    
+        // go back N
+        else if(args.length == 6){
+            int window_size = Integer.parseInt(args[5]);
+        }
+    
+
+    }
     
 }
+
